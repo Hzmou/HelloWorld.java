0dd41bfb8654eb91183f347193ec25b9f8c75848
HZmou <zmou31@gmail.com>
2021-02-23 07:46:01 -0500

    Solved displaying the current date and time to user issue.

--- helloworld.java
+++ helloworld.java
@@ -1,12 +1,11 @@
 
-
 import java.util.*;
-
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 public class helloworld {
 
-	
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 		
 		
 		System.out.println(" Hello World! ");
@@ -18,28 +17,29 @@ public class helloworld {
 		
                // this is the second message.
           System.out.println(" this is the final Comment");
-
-
-
-           //Saying Hi to both Alice and Bob.
+            //Saying Hi to both Alice and Bob.
 
            System.out.println("Hi to Alice and Bob!");
            
                  //Alice code.
                System.out.println("Hi Alice");
-
-
           // saying hi to Denise.
 
-          System.out.println(" Hi Denise!)
+          System.out.println(" Hi Denise!");
 
-         // Saying Hi to Ester.
-         System.out.println("Hi Ester!");
+           // Saying Hi to Ester.
+           System.out.println("Hi Ester!");
 
+       /* Displaying the Current date to the user.*/
+
+       DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+       LocalDateTime now = LocalDateTime.now();  
+        System.out.println(dtf.format(now));  
+
+         
 
            
 
 	}
-	
-	
+
 }
